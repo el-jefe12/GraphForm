4bf963dd26038c3ad8c3f8882759b5ad27fe76eb
El-Jefe <STEVEKLEIN@seznam.cz>
2024-02-18 19:39:23 +0100

    init 1

---
+++ Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace GraphForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.XMLExportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CSVExportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importFromToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.XMLImportToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.CSVImportToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.yearDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.monthDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.valueDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.valuesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.valuesBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.valuesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.valuesBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.yearDataGridViewTextBoxColumn,
+            this.monthDataGridViewTextBoxColumn,
+            this.valueDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.valuesBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(343, 318);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Location = new System.Drawing.Point(361, 27);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(427, 318);
+            this.cartesianChart1.TabIndex = 3;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.toolsToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("saveAsToolStripMenuItem.Image")));
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveAsToolStripMenuItem.Text = "Save as";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.printToolStripMenuItem,
+            this.exportToToolStripMenuItem,
+            this.importFromToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.printToolStripMenuItem.Text = "Print";
+            this.printToolStripMenuItem.Click += new System.EventHandler(this.printToolStripMenuItem_Click);
+            // 
+            // exportToToolStripMenuItem
+            // 
+            this.exportToToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.XMLExportToolStripMenuItem,
+            this.CSVExportToolStripMenuItem});
+            this.exportToToolStripMenuItem.Name = "exportToToolStripMenuItem";
+            this.exportToToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.exportToToolStripMenuItem.Text = "Export to";
+            // 
+            // XMLExportToolStripMenuItem
+            // 
+            this.XMLExportToolStripMenuItem.Name = "XMLExportToolStripMenuItem";
+            this.XMLExportToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.XMLExportToolStripMenuItem.Text = "XML";
+            this.XMLExportToolStripMenuItem.Click += new System.EventHandler(this.XMLExportToolStripMenuItem_Click);
+            // 
+            // CSVExportToolStripMenuItem
+            // 
+            this.CSVExportToolStripMenuItem.Name = "CSVExportToolStripMenuItem";
+            this.CSVExportToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.CSVExportToolStripMenuItem.Text = "CSV";
+            this.CSVExportToolStripMenuItem.Click += new System.EventHandler(this.CSVExportToolStripMenuItem_Click);
+            // 
+            // importFromToolStripMenuItem
+            // 
+            this.importFromToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.XMLImportToolStripMenuItem1,
+            this.CSVImportToolStripMenuItem1});
+            this.importFromToolStripMenuItem.Name = "importFromToolStripMenuItem";
+            this.importFromToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.importFromToolStripMenuItem.Text = "Import from";
+            // 
+            // XMLImportToolStripMenuItem1
+            // 
+            this.XMLImportToolStripMenuItem1.Name = "XMLImportToolStripMenuItem1";
+            this.XMLImportToolStripMenuItem1.Size = new System.Drawing.Size(98, 22);
+            this.XMLImportToolStripMenuItem1.Text = "XML";
+            this.XMLImportToolStripMenuItem1.Click += new System.EventHandler(this.XMLImportToolStripMenuItem1_Click);
+            // 
+            // CSVImportToolStripMenuItem1
+            // 
+            this.CSVImportToolStripMenuItem1.Name = "CSVImportToolStripMenuItem1";
+            this.CSVImportToolStripMenuItem1.Size = new System.Drawing.Size(98, 22);
+            this.CSVImportToolStripMenuItem1.Text = "CSV";
+            this.CSVImportToolStripMenuItem1.Click += new System.EventHandler(this.CSVImportToolStripMenuItem1_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // yearDataGridViewTextBoxColumn
+            // 
+            this.yearDataGridViewTextBoxColumn.DataPropertyName = "Year";
+            this.yearDataGridViewTextBoxColumn.HeaderText = "Year";
+            this.yearDataGridViewTextBoxColumn.Name = "yearDataGridViewTextBoxColumn";
+            // 
+            // monthDataGridViewTextBoxColumn
+            // 
+            this.monthDataGridViewTextBoxColumn.DataPropertyName = "Month";
+            this.monthDataGridViewTextBoxColumn.HeaderText = "Month";
+            this.monthDataGridViewTextBoxColumn.Name = "monthDataGridViewTextBoxColumn";
+            // 
+            // valueDataGridViewTextBoxColumn
+            // 
+            this.valueDataGridViewTextBoxColumn.DataPropertyName = "Value";
+            this.valueDataGridViewTextBoxColumn.HeaderText = "Value";
+            this.valueDataGridViewTextBoxColumn.Name = "valueDataGridViewTextBoxColumn";
+            // 
+            // valuesBindingSource
+            // 
+            this.valuesBindingSource.DataSource = typeof(GraphForm.Values);
+            // 
+            // valuesBindingSource1
+            // 
+            this.valuesBindingSource1.DataSource = typeof(GraphForm.Values);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 498);
+            this.Controls.Add(this.cartesianChart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.valuesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.valuesBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource valuesBindingSource;
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn yearDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn monthDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn valueDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource valuesBindingSource1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.ToolStripMenuItem exportToToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importFromToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem XMLExportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem CSVExportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem XMLImportToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem CSVImportToolStripMenuItem1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,348 @@
+﻿using LiveCharts;
+using LiveCharts.Definitions.Charts;
+using LiveCharts.WinForms;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Xml.Serialization;
+
+namespace GraphForm
+{
+    public partial class Form1 : Form
+    {
+        public string genericFileName = "file";
+
+        public enum FileType
+        {
+            none,
+            XML,
+            CSV
+        }
+
+        public static FileType filetype = FileType.none;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+        public void SaveGraph()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PNG Image|*.png|JPEG Image|*.jpg|Bitmap Image|*.bmp";
+            saveFileDialog.FileName = "graph.png"; // Set the default file name here
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Create a bitmap to render the LiveCharts chart
+                Bitmap bmp = new Bitmap(cartesianChart1.Width, cartesianChart1.Height);
+                cartesianChart1.DrawToBitmap(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
+
+                // Get the selected file extension from the save dialog
+                string fileExtension = Path.GetExtension(saveFileDialog.FileName);
+
+                // Save the bitmap to the selected file
+                switch (fileExtension.ToLower())
+                {
+                    case ".png":
+                        bmp.Save(saveFileDialog.FileName, ImageFormat.Png);
+                        break;
+                    case ".jpg":
+                        bmp.Save(saveFileDialog.FileName, ImageFormat.Jpeg);
+                        break;
+                    case ".bmp":
+                        bmp.Save(saveFileDialog.FileName, ImageFormat.Bmp);
+                        break;
+                    default:
+                        // Handle unsupported file types or display an error message
+                        MessageBox.Show("Unsupported file type", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                }
+            }
+        }
+    
+
+        public void UpdateGraph()
+        {
+            cartesianChart1.Series.Clear();
+            LiveCharts.SeriesCollection sc = new LiveCharts.SeriesCollection();
+
+            var years = (from o in valuesBindingSource.DataSource as List<Values> select new { Year = o.Year }).Distinct();
+
+            foreach (var y in years)
+            {
+                List<double> values = new List<double>();
+
+                for (int month = 1; month <= 12; month++)
+                {
+                    double val = 0;
+
+                    var data = from o in valuesBindingSource.DataSource as List<Values>
+                               where o.Year.Equals(y.Year) && o.Month.Equals(month)
+                               orderby o.Month ascending
+                               select new { o.Value, o.Month };
+
+                    // Sum the values for the same month
+                    foreach (var item in data)
+                    {
+                        val += item.Value;
+                    }
+
+                    values.Add(val);
+                }
+
+                sc.Add(new LineSeries()
+                {
+                    Title = y.Year.ToString(),
+                    Values = new ChartValues<double>(values)
+                });
+            }
+
+            cartesianChart1.Series = sc;
+        }
+
+
+        private void PrintGraph(object sender, PrintPageEventArgs e) // should prnt i guess
+        {
+            Bitmap bmp = new Bitmap(cartesianChart1.Width, cartesianChart1.Height);
+            cartesianChart1.DrawToBitmap(bmp, cartesianChart1.ClientRectangle);
+
+            e.Graphics.DrawImage(bmp, 0,0);
+        }
+
+        public void ImportData(string filename, FileType filetype)
+        {
+            try
+            {
+                if (filetype == FileType.XML)
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(List<Values>));
+
+                    using (TextReader reader = new StreamReader(filename))
+                    {
+                        List<Values> importedData = (List<Values>)serializer.Deserialize(reader);
+                        valuesBindingSource.DataSource = importedData;
+                        UpdateGraph();
+                    }
+                }
+                else if (filetype == FileType.CSV)
+                {
+                    List<Values> importedData = new List<Values>();
+
+                    using (var reader = new StreamReader(filename))
+                    {
+                        while (!reader.EndOfStream)
+                        {
+                            var line = reader.ReadLine();
+                            var values = line.Split(',');
+
+                            if (values.Length == 3)
+                            {
+                                int year, month;
+                                double value;
+
+                                if (int.TryParse(values[0], out year) && int.TryParse(values[1], out month) && double.TryParse(values[2], out value))
+                                {
+                                    importedData.Add(new Values { Year = year, Month = month, Value = value });
+                                }
+                            }
+                            // Handle incorrect CSV format or display an error message
+                        }
+                    }
+
+                    valuesBindingSource.DataSource = importedData;
+                    UpdateGraph();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid file type.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error while importing data: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        public void ExportData(FileType filetype)
+        {
+            List<Values> dataList = valuesBindingSource.DataSource as List<Values>;
+
+            if (dataList == null)
+            {
+                MessageBox.Show("No data to export.");
+                return;
+            }
+
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                if (filetype == FileType.XML)
+                {
+                    saveFileDialog.Filter = "XML Files|*.xml";
+                    saveFileDialog.FileName = "data.xml"; // Set the default file name here
+                }
+                else if (filetype == FileType.CSV)
+                {
+                    saveFileDialog.Filter = "CSV Files|*.csv";
+                    saveFileDialog.FileName = "data.csv"; // Set the default file name here
+                }
+                else
+                {
+                    MessageBox.Show("Invalid file type.");
+                    return;
+                }
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = saveFileDialog.FileName;
+
+                    if (filetype == FileType.XML)
+                    {
+                        XmlSerializer serializer = new XmlSerializer(typeof(List<Values>));
+
+                        using (TextWriter writer = new StreamWriter(filePath))
+                        {
+                            serializer.Serialize(writer, dataList);
+                        }
+                    }
+                    else if (filetype == FileType.CSV)
+                    {
+                        StringBuilder csvContent = new StringBuilder();
+
+                        // Add header
+                        csvContent.AppendLine("Year,Month,Value");
+
+                        foreach (var data in dataList)
+                        {
+                            // Format the line as per your CSV structure
+                            csvContent.AppendLine($"{data.Year},{data.Month},{data.Value}");
+                        }
+
+                        File.WriteAllText(filePath, csvContent.ToString());
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid file type.");
+                        return;
+                    }
+
+                    MessageBox.Show($"Data exported to {filePath}");
+                }
+            }
+        }
+
+
+        private void pieChart1_ChildChanged(object sender, System.Windows.Forms.Integration.ChildChangedEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            valuesBindingSource.DataSource = new List<Values>();
+
+            cartesianChart1.AxisX.Add(new LiveCharts.Wpf.Axis
+            {
+                Title = "Month",
+            });
+            cartesianChart1.AxisY.Add(new LiveCharts.Wpf.Axis
+            {
+                Title = "Value",
+            });
+
+            cartesianChart1.LegendLocation = LiveCharts.LegendLocation.Right;
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (valuesBindingSource.DataSource == null)
+            {
+                return;
+            }
+
+            UpdateGraph();
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveGraph();
+        }
+
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var pDg = new PrintDialog();
+            var pDoc = new PrintDocument();
+
+            pDoc.PrintPage += new PrintPageEventHandler(PrintGraph);
+
+            if (pDg.ShowDialog() == DialogResult.OK)
+            {
+                pDoc.Print();
+            }
+
+
+        }
+
+        private void XMLExportToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            ExportData(FileType.XML);
+        }
+
+        private void CSVExportToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            ExportData(FileType.CSV);
+        }
+
+        private void XMLImportToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "XML Files|*.xml";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                ImportData(openFileDialog.FileName, FileType.XML);
+            }
+        }
+
+        private void CSVImportToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "CSV Files|*.csv";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                ImportData(openFileDialog.FileName, FileType.CSV);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Made by: Jan Klein\n\r\n\rRegistered Trademark 2024", "About");
+        }
+    }
+}
